c843f3e2a2ebeb64d278234ea48ba76c0333b4d7
苶雉 <1835200937@qq.com>
2024-02-01 22:14:40 +0800

    添加项目文件。

---
+++ WinStudent/FrmLogin.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace WinStudent
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtUserPwd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtUserPwd);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnExit);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtUserName);
+            this.groupBox1.Controls.Add(this.btnlogin);
+            this.groupBox1.Location = new System.Drawing.Point(155, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(490, 403);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtUserPwd
+            // 
+            this.txtUserPwd.Location = new System.Drawing.Point(178, 173);
+            this.txtUserPwd.Name = "txtUserPwd";
+            this.txtUserPwd.Size = new System.Drawing.Size(164, 28);
+            this.txtUserPwd.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(90, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "密码:";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(210, 271);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(132, 45);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "退出";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "账号:";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(178, 120);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(164, 28);
+            this.txtUserName.TabIndex = 2;
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Location = new System.Drawing.Point(54, 271);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(132, 45);
+            this.btnlogin.TabIndex = 0;
+            this.btnlogin.Text = "登录";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "FrmLogin";
+            this.Text = "登录页面";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtUserPwd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Button btnlogin;
+    }
+}
+
---
+++ WinStudent/FrmLogin.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinStudent
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            //获取用户输入信息  
+            string uName = txtUserName.Text.Trim();
+            string uPwd = txtUserPwd.Text.Trim();
+            //判断是否为空    
+            if (string.IsNullOrEmpty(uName))
+            {
+                MessageBox.Show("请输入账号!", "登陆提示", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                txtUserName.Focus();
+                return;
+            }
+            if (string.IsNullOrEmpty(uPwd))
+            {
+                MessageBox.Show("请输入密码!", "登陆提示", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                txtUserPwd.Focus();
+                return;
+            }
+            {
+                //建立与数据库的链接
+                //连接字符串 -- 钥匙
+                //string connSting = "server=.;database=test;Integrated  Security=true"; 
+                //字符串 Window 身份验证
+                string connString = "server=LAPTOP-D9MFPFTD;database=StudentNewDB;Persist Security Info=True;User ID=sa;Password=123456;";//Sql 身份验证
+                SqlConnection conn = new SqlConnection(connString);
+
+                //打开连接
+                conn.Open(); ;  //最晚打开 最早关闭
+                //写查询语句
+                string sql = "select count(1) from UserInfo where UserName='" + uName + "'and userpwd='" + uPwd + "'";
+                //创建Command对象
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+
+                //执行命令
+                object o = cmd.ExecuteScalar();//执行查询，返回结果集第一行第一列的值，忽略其他行或列
+                //关闭连接
+                conn.Close();
+                //处理结果
+                if (o == null || o == DBNull.Value || ((int)o) == 0)
+                {
+                    MessageBox.Show("登录账号或密码有错，请检查！", "登陆提示", MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                    return;
+                }
+                else;
+            }
+            {
+                MessageBox.Show("登录成功! ", "登陆提示", MessageBoxButtons.OK,
+                          MessageBoxIcon.Information);
+                //转到主页面
+            }
+
+            //返回的结果进行不同的提示
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            //Application.Exit()
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
