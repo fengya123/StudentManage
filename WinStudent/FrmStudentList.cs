5ca7e51b2e4ceddb2f5d481a755f8168f84bacb2
苶雉 <1835200937@qq.com>
2024-03-20 07:46:25 +0800

    3.17

--- WinStudent/FrmStudentList.cs
+++ WinStudent/FrmStudentList.cs
@@ -102,7 +102,8 @@ namespace WinStudent
             //查询sql
             string sql = "select StuId,StuName,ClassName,GradeName,Sex,Phone from StudentInfo s " +
                 "inner join ClassInfo c on c.ClassId=s.ClassId " +
-                "inner join GradeInfo g on g.GradeId=c.GradeId";
+                "inner join GradeInfo g on g.GradeId=c.GradeId " +
+                " where s.IsDeleted=0";
             sql += " where 1=1";
             if(classId>0)
             {
@@ -112,6 +113,7 @@ namespace WinStudent
             {
                 sql += " and StuName like @StuName";
             }
+            sql += " where s.IsDeleted=0";
             sql += " order by StuId";
 
             SqlParameter[] paras =
@@ -157,6 +159,36 @@ namespace WinStudent
                 else if (cell is DataGridViewLinkCell && cell.FormattedValue.ToString() == "删除")
                 {
                     //删除操作 
+                    if(MessageBox.Show("您确定要删除该学生信息吗？","删除学生提示",
+                        MessageBoxButtons.YesNo,MessageBoxIcon.Question)
+                        ==DialogResult.Yes)
+                    {   
+                        //获取行数据
+                        DataRow dr = (dgvStudents.Rows[e.RowIndex].DataBoundItem as DataRowView).Row;
+                        int stuId = int.Parse(dr["StuId"].ToString());
+                        //假删除 IsDeleted 0 1
+                        string sqlDe10 = "update StudentInfo set IsDeleted=1 where StuId=@StuId";
+                        SqlParameter para = new SqlParameter("@StuId", stuId);
+                        int count = SqlHelper.ExecuteNonQuery(sqlDe10, para);
+                        if(count>0)
+                        {
+                            MessageBox.Show("该学生信息删除成功!", "删除学生提示", 
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            //DataGridView数据并没有刷新，手动刷新
+                            DataTable dtStudents = (DataTable)dgvStudents.DataSource;
+                            dgvStudents.DataSource = null;
+                            dtStudents.Rows.Remove(dr);
+                            dgvStudents.DataSource = dtStudents;
+                        }
+                        else
+                        {
+                            MessageBox.Show("该学生信息删除失败!", "删除学生提示",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+
+                        //真删除 delete where StuId
+                    }
                 }
             }
 
